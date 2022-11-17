443d933edad124df69dfdbc106d4ae92d8b7c42c
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-17 13:44:34 +0000

    started adding profile card data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,20 @@
+import FooterLinks from "./Social-Links";
+
 const Card = () => {
     return (
-        <div className="card">
-            <h1>test</h1>
+        <div className="Card">
+            <div className="ProfilePicture"></div>
+            <div className="ProfileSnapshot">
+                <h1 className="ProfileName">Laura Smith</h1>
+                <h2 className="ProfileTitle">Frontend Developer</h2>
+                <p classname="ProfileWebsiteLink">laurasmith.website</p>
+            </div>
+            <div className="ContactButtons">
+                <button>Email</button>
+                <button>LinkedIn</button>
+            </div>
+
+            <FooterLinks />
         </div>
     )
 }
---
+++ src/components/Social-Links.jsx
@@ -0,0 +1,14 @@
+import faceBookIcon from "../mockups/Facebook-Icon.svg"
+import githubIcon from "../mockups/GitHub-Icon.svg"
+import instagramIcon from "../mockups/Instagram-Icon.svg"
+import twitterIcon from "../mockups/Twitter-Icon.svg"
+
+const FooterLinks = () => {
+    return (
+        <div className="FooterLinks">
+
+        </div>
+    )
+}
+
+export default FooterLinks;
