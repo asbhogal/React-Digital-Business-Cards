8bad48c7c86fc0ca94ffb9c973e4c600787d1439
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-17 12:18:46 +0000

    added components and configured exports

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import "../scss/index.scss";
+import Card from "./Card";
+
+const App = () => {
+    return (
+        <div className="Container">
+            <Card />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+    return (
+        <div className="card">
+            <h1>test</h1>
+        </div>
+    )
+}
+
+export default Card;
