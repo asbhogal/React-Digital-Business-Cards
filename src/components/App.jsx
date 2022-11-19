3f16f294b515521e192f4a32396b1becf8a84e4e
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-11-19 22:47:22 +0000

    added props to card elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,61 @@ import Card from "./Card";
 const App = () => {
     return (
         <div className="Container">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <Card 
+                ProfileMainSectionStyle="ProfileMainSection"
+                ProfileSnapshotStyle="ProfileSnapshot"
+                ProfileNameStyle="ProfileName"
+                ProfileTitleStyle="ProfileTitle"
+                ProfileWebsiteLinkStyle="ProfileWebsiteLink"
+
+                ProfileSnapshotInfoStyle="ProfileSnapshotInfo"
+                ProfileAboutHeadingStyle="ProfileAbout"
+                ProfileAboutTextStyle="ProfileAboutText"
+                ProfileInterestsHeadingStyle="ProfileInterests"
+                ProfileInterestsTextStyle="ProfileAboutText"
+            />
+            <Card 
+                ProfileMainSectionStyle="ProfileMainSection"
+                ProfileSnapshotStyle="ProfileSnapshot"
+                ProfileNameStyle="ProfileName"
+                ProfileTitleStyle="ProfileTitle"
+                ProfileWebsiteLinkStyle="ProfileWebsiteLink"
+                
+                ProfileSnapshotInfoStyle="ProfileSnapshotInfo"
+                ProfileAboutHeadingStyle="ProfileAbout"
+                ProfileAboutTextStyle="ProfileAboutText"
+                ProfileInterestsHeadingStyle="ProfileInterests"
+                ProfileInterestsTextStyle="ProfileAboutText"
+            />
+            <Card 
+                ProfileMainSectionStyle="ProfileMainSection-Light"
+                ProfileSnapshotStyle="ProfileSnapshot-Light"
+                ProfileNameStyle="ProfileName-Light"
+                ProfileTitleStyle="ProfileTitle-Light"
+                ProfileWebsiteLinkStyle="ProfileWebsiteLink-Light" 
+
+                ProfileSnapshotInfoStyle="ProfileSnapshotInfo-Light"
+                ProfileAboutHeadingStyle="ProfileAbout-Light"
+                ProfileAboutTextStyle="ProfileAboutText-Light"
+                ProfileInterestsHeadingStyle="ProfileInterests-Light"
+                ProfileInterestsTextStyle="ProfileAboutText-Light"
+            />
+            <Card 
+                ProfileMainSectionStyle="ProfileMainSection-Light"
+                ProfileSnapshotStyle="ProfileSnapshot-Light"
+                ProfileNameStyle="ProfileName-Light"
+                ProfileTitleStyle="ProfileTitle-Light"
+                ProfileWebsiteLinkStyle="ProfileWebsiteLink-Light" 
+
+                ProfileSnapshotInfoStyle="ProfileSnapshotInfo-Light"
+                ProfileAboutHeadingStyle="ProfileAbout-Light"
+                ProfileAboutTextStyle="ProfileAboutText-Light"
+                ProfileInterestsHeadingStyle="ProfileInterests-Light"
+                ProfileInterestsTextStyle="ProfileAboutText-Light"
+            />
         </div>
     )
 }
 
 export default App;
+
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,17 +3,18 @@ import emailIcon from "../mockups/Mail-Icon.svg";
 import linkedinMiniIcon from "../mockups/LinkedIn-Mini-Icon.svg";
 import FooterLinks from "./FooterLinks";
 
-const Card = () => {
+const Card = (props) => {
+    console.log(props);
     return (
         <div className="Card">
             <div className="ProfilePicture">
                 <img src={ profilePicture }></img>
             </div>
-            <section className="ProfileMainSection">
-            <div className="ProfileSnapshot">
-                <h1 className="ProfileName">Laura Smith</h1>
-                <h2 className="ProfileTitle">Frontend Developer</h2>
-                <p className="ProfileWebsiteLink">laurasmith.website</p>
+            <section className={props.ProfileMainSectionStyle}>
+            <div className={props.ProfileSnapshotStyle}>
+                <h1 className={props.ProfileNameStyle}>Laura Smith</h1>
+                <h2 className={props.ProfileTitleStyle}>Frontend Developer</h2>
+                <p className={props.ProfileWebsiteLinkStyle}>laurasmith.website</p>
             </div>
             <div className="ContactButtons">
                 <button className="EmailButton">
@@ -25,11 +26,11 @@ const Card = () => {
                 <span className="LinkedInButtonText">LinkedIn</span>
                 </button>
             </div>
-            <div className="ProfileSnapshotInfo">
-                <h3 className="ProfileAbout">About</h3>
-                <p className="ProfileAboutText">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
-                <h3 className="ProfileInterests">Interests</h3>
-                <p className="ProfileAboutText">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <div className={props.ProfileSnapshotInfoStyle}>
+                <h3 className={props.ProfileAboutHeadingStyle}>About</h3>
+                <p className={props.ProfileAboutTextStyle}>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+                <h3 className={props.ProfileInterestsHeadingStyle}>Interests</h3>
+                <p className={props.ProfileInterestsTextStyle}>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
             </div>
             </section>
             <FooterLinks />
